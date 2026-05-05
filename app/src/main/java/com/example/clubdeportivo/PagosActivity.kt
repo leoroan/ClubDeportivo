9c5811ffa64f46c55f86e7f5f558d19a2b39ceda
Leandro Maselli <leoroan@gmail.com>
2026-05-05 00:47:12 -0300

    agrgada la vista para pagos y perfiles de usuario  -base

---
+++ app/src/main/java/com/example/clubdeportivo/PagosActivity.kt
@@ -0,0 +1,11 @@
+package com.example.clubdeportivo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PagosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pagos)
+    }
+}
---
+++ app/src/main/java/com/example/clubdeportivo/PerfilUsuarioActivity.kt
@@ -0,0 +1,11 @@
+package com.example.clubdeportivo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PerfilUsuarioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_perfil_usuario)
+    }
+}
